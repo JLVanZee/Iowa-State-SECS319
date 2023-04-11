4bfedf2e3885ee933bf89662d9de5846a57fa4e3
Jaret Van Zee <vanzeejaret@gmail.com>
2023-04-11 14:35:18 -0500

    Payment added basic functionality, fixes needed

--- script.js
+++ script.js
@@ -113,3 +113,23 @@ function search() {
     
     console.log("done searching");
 }
+
+
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()

d3fba3b17bc8609c8a5644369183b3218fb32dd5
Jaret Van Zee <vanzeejaret@gmail.com>
2023-04-30 16:35:03 -0500

    added in delete and insert methods

--- mern_backend/index.js
+++ mern_backend/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+const Product = require("./dataSchema.js");
 
 
 app.use(express.json());
@@ -22,9 +23,47 @@ app.listen(port, () => {
     console.log(`App listening at http://%s:%s`, host, port);
 });
 
-app.get("/", async (req, resp) => {
-    const query = {};
-    const allProducts = await Product.find(query);
-    console.log(allProducts);
-    resp.send(allProducts);
+app.get("/:id", async (req, resp) => {
+    const id = req.params.id;
+    const query = {_id: id};
+    const oneProduct = await Product.findOne(query);
+    console.log(oneProduct);
+    resp.send(oneProduct);
+});
+
+app.post("/insert", async (req, res) => {
+    console.log(req.body);
+    const p_id = req.body._id;
+    const ptitle = req.body.title;
+    const pprice = req.body.price;
+    const pdescription = req.body.description;
+    const pcategory = req.body.category;
+    const pimage = req.body.image;
+    const prate = req.body.rating.rate;
+    const pcount = req.body.rating.count;
+    
+    try {
+        // await formData.save();
+        await Product.create(formData);
+        const messageResponse = { message: `Product ${p_id} added correctly` };
+        res.send(JSON.stringify(messageResponse));
+    } catch (err) {
+        console.log("Error while adding a new product:" + err);
+    }
+});
+
+app.delete("/delete", async (req, res) => {
+    console.log("Delete :", req.body);
+    
+    try {
+        const query = { _id: req.body._id };
+        await Product.deleteOne(query);
+
+        const messageResponse = {
+            message: `Product ${req.body._id} deleted correctly`,
+        };
+        res.send(JSON.stringify(messageResponse));
+    } catch (err) {
+        console.log("Error while deleting :" + p_id + " " + err);
+    }
 });
